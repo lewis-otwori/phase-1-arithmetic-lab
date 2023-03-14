d2d248e6e9bb8d1f028eeba38a5114c0d7a5c06e
lewis-otwori <lewisotwori@gmail.com>
2023-03-14 05:21:07 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 100) + 1; 
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+if (mod === 4) {
+  console.log("The remainder of dividing num3 by num4 is 4.");
+}
+
+const numbers = [5, 10, 15, 20, 17, 3, 9];
+const max = Math.max(...numbers);
+console.log(max);
